7c535cd0b8d85e7db63bc15920242d2c7b156ad7
trachemys-scripta57 <6416417@gmail.com>
2022-09-02 19:22:22 +0300

    Повторно восстановил утерянную версию 31.08.22 Добавил доступ к классу через интерфейс UserDao

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,46 +1,17 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
 import java.util.List;
 
 
 public class UserServiceImpl implements UserService {
-//    private final UserDaoJDBCImpl dao = new UserDaoJDBCImpl(Util.getConnection());
-UserDao userDao = new UserDao() {
-    @Override
+    private final UserDao userDao = new UserDaoJDBCImpl(Util.getConnection());
+
     public void createUsersTable() {
-
-    }
-
-    @Override
-    public void dropUsersTable() {
-
-    }
-
-    @Override
-    public void saveUser(String name, String lastName, byte age) {
-
-    }
-
-    @Override
-    public void removeUserById(long id) {
-
-    }
-
-    @Override
-    public List<User> getAllUsers() {
-        return null;
-    }
-
-    @Override
-    public void cleanUsersTable() {
-
-    }
-};
-    public void createUsersTable() {
-
         userDao.createUsersTable();
     }
 
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -17,7 +17,7 @@ public class Util {
             Driver driver = new com.mysql.cj.jdbc.Driver();
             DriverManager.registerDriver(driver);
             connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
-
+            connection.setAutoCommit(false);
             if (!connection.isClosed()) {
                 System.out.println("Соединение установлено");
             }
